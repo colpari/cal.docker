cfa6d7177304eed09aef119c6c6d4c1aa4500dc5
Frank Fricke <frank.fricke@colpari.cx>
2023-06-22 14:42:53 +0200

    update build

--- build.sh
+++ build.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
+set -e
 
 echo -n "Enter custom NEXTAUTH_SECRET value - leave blank to auto-generate: "; read NEXTAUTH_SECRET
 if [ -z "$NEXTAUTH_SECRET" ]
 then
 	NEXTAUTH_SECRET=$(openssl rand -base64 32)
-	echo "NEXTAUTH_SECRET=$NEXTAUTH_SECRET"
 fi
 
 echo -n "Enter custom CALENDSO_ENCRYPTION_KEY value - leave blank to auto-generate: "; read CALENDSO_ENCRYPTION_KEY
 if [ -z "$CALENDSO_ENCRYPTION_KEY" ]
 then
 	CALENDSO_ENCRYPTION_KEY=$(openssl rand -base64 24)
-	echo "CALENDSO_ENCRYPTION_KEY=$CALENDSO_ENCRYPTION_KEY"
 fi
 
-docker build \
-	--build-arg "NEXTAUTH_SECRET=$NEXTAUTH_SECRET" \
-	--build-arg "CALENDSO_ENCRYPTION_KEY=$CALENDSO_ENCRYPTION_KEY" \
-	-t containers.colpari.dev/colpari-hello/cal.cx .
+egrep -v 'CALENDSO_ENCRYPTION_KEY|NEXTAUTH_SECRET' .env > .env2
+echo "NEXTAUTH_SECRET=$NEXTAUTH_SECRET" >> .env2
+echo "CALENDSO_ENCRYPTION_KEY=$CALENDSO_ENCRYPTION_KEY" >> .env2
+mv -vf .env2 .env
+
+sudo docker-compose up -d database
+set +e
+sudo docker-compose build cal
+
+sudo docker-compose down -v
+
+echo "NEXTAUTH_SECRET=$NEXTAUTH_SECRET"
+echo "CALENDSO_ENCRYPTION_KEY=$CALENDSO_ENCRYPTION_KEY"
+
